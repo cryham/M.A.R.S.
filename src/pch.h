4e3b9dfe2d2f28b0e8502cef1d98302f566daadf
cryham <cryham@gmail.com>
2025-04-20 15:41:30 +0200

    cmake fix, add pch.h precompiled header, faster build

---
+++ src/pch.h
@@ -0,0 +1,30 @@
+#pragma once
+//  precompiled header, with common includes, for faster build
+
+#include <iostream>
+//#include <fstream>
+//#include <filesystem>
+
+#include <cstdio>
+#include <ctime>
+
+#include <string>
+#include <sstream>
+//#include <cstring>
+
+#include <vector>
+#include <map>
+#include <list>
+#include <set>
+
+#include <cmath>
+#include <cfloat>
+
+
+#include <GL/gl.h>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/PrimitiveType.hpp>
+#include <SFML/OpenGL.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
